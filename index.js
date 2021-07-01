cdfeda9a4cb7f8f7e8416fad0e968cf501bfebeb
brett <b.woltz123@gmail.com>
2021-06-30 22:42:19 -0700

    Express now serves react pages

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static("client/public"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.get("/api/contact", (req, res) => {
     console.log(req);
